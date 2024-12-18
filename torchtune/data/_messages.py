6bd8f6b2f0bcb0fa1a85f2c4fcfa77bf8ac1f62d
AdalAbilbekov <adalabilbekov@gmail.com>
2024-12-18 07:34:42 +0000

    Feat: add custom instruct prompt template.

--- torchtune/data/_messages.py
+++ torchtune/data/_messages.py
@@ -704,6 +704,11 @@ class AlpacaToMessages(Transform):
                 "Write a response that appropriately completes the request.\n\n"
                 "### Instruction:\n{instruction}\n\n### Response:\n"
             ),
+            "prompt_issai": (
+                "Below is an instruction that describes a task. "
+                "Write a response that appropriately completes the request.\n\n"
+                "### Instruction:\n{instruction}\n{input}\n\n### Response:\n"
+            )
         }
 
     def __call__(self, sample: Mapping[str, Any]) -> Mapping[str, Any]:
@@ -712,13 +717,8 @@ class AlpacaToMessages(Transform):
         key_instruction = column_map.get("instruction", "instruction")
         key_output = column_map.get("output", "output")
 
-        if key_input in sample and sample[key_input]:
-            prompt = self.template["prompt_input"].format(
+        prompt = self.template["prompt_issai"].format(
                 instruction=sample[key_instruction], input=sample[key_input]
-            )
-        else:
-            prompt = self.template["prompt_no_input"].format(
-                instruction=sample[key_instruction]
             )
 
         messages = [
